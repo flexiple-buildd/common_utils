ec63ee3e37edae80af9f49edddcac4862147efec
Aarav <aaravkhokhar@yahoo.in>
2024-05-15 19:58:38 +0530

    (fix): use parametrize to generate id, add prefix to id, throw error if same ids are found

--- lib/common_utils/toc_extractor.rb
+++ lib/common_utils/toc_extractor.rb
@@ -17,15 +17,24 @@ class TOCExtractor
   def self.extract_toc_from_headings(doc)
     toc_items = []
     current_parent = nil
+    hrefs = []
 
     doc.css('h2, h3').each do |heading|
       heading_id = heading['id'] || generate_heading_id(heading.text)
       if heading.name == 'h2'
         current_parent = { text: heading.text.strip, href: "##{heading_id}", sub_items: [] }
         toc_items << current_parent
+        if hrefs.include?(current_parent[:href])
+          raise "Duplicate href found: #{current_parent[:href]}"
+        end
+        hrefs << current_parent[:href]
       elsif heading.name == 'h3' && current_parent
         sub_item = { text: heading.text.strip, href: "##{heading_id}" }
+        if hrefs.include?(sub_item[:href])
+          raise "Duplicate href found: #{sub_item[:href]}"
+        end
         current_parent[:sub_items] << sub_item
+        hrefs << sub_item[:href]
       end
     end
 
@@ -42,11 +51,11 @@ class TOCExtractor
 
   def self.add_ids_to_headings(doc)
     doc.css('h2, h3').each do |heading|
-      heading['id'] ||= generate_heading_id(heading.text)
+      heading['id'] ||= generate_heading_id(heading)
     end
   end
 
-  def self.generate_heading_id(text)
-    text.downcase.strip.gsub(/\s+/, '-')
+  def self.generate_heading_id(heading)
+    "#{heading.name}-" + heading.text.parameterize
   end
 end
