fe3441057c18a6bff2b6a33e885609ff8b00ac5c
Aarav <aaravkhokhar@yahoo.in>
2024-07-26 17:59:30 +0530

    (fix) improve toc extractor

--- lib/common_utils/toc_extractor.rb
+++ lib/common_utils/toc_extractor.rb
@@ -17,34 +17,32 @@ class TOCExtractor
   def self.extract_toc_from_headings(doc)
     toc_items = []
     current_parent = nil
-    hrefs = []
+    hrefs = {}
 
     doc.css('h2, h3').each do |heading|
-      heading_id = heading['id'] || generate_heading_id(heading.text)
+      heading_id = heading['id'] || generate_heading_id(heading)
+      toc_item = create_toc_item(heading, heading_id, hrefs)
+
       if heading.name == 'h2'
-        current_parent = { text: heading.text.strip, href: "##{heading_id}", sub_items: [] }
+        current_parent = toc_item
         toc_items << current_parent
-        countOfHref = hrefs.count(current_parent[:href])
-        if(countOfHref > 0)
-          current_parent[:href] = "#{current_parent[:href]}-#{countOfHref}"
-          heading['id'] = "#{heading_id}-#{countOfHref}"
-        else
-          hrefs << current_parent[:href]
-        end
       elsif heading.name == 'h3' && current_parent
-        sub_item = { text: heading.text.strip, href: "##{heading_id}" }
-        if hrefs.count(sub_item[:href]) > 0
-          sub_item[:href] = "#{sub_item[:href]}-#{hrefs.count(sub_item[:href])}"
-          heading['id'] = "#{heading_id}-#{hrefs.count(sub_item[:href])}"
-        else
-          hrefs << sub_item[:href]
-        end
-        current_parent[:sub_items] << sub_item
-        hrefs << sub_item[:href]
+        current_parent[:sub_items] << toc_item
       end
     end
 
     toc_items
+  end
+
+  def self.create_toc_item(heading, heading_id, hrefs)
+    href = "##{heading_id}"
+    count_of_href = hrefs[href] || 0
+    if count_of_href > 0
+      href = "#{href}-#{count_of_href}"
+      heading['id'] = "#{heading_id}-#{count_of_href}"
+    end
+    hrefs[href] = count_of_href + 1
+    { text: heading.text.strip, href: href, sub_items: [] }
   end
 
   def self.remove_toc_from_content(doc)
